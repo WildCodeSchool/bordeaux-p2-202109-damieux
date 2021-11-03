3a3206380f506bfdee6af09ee17fa79352e8f0b8
CaronGregoire <gregoirecaron33@gmail.com>
2021-11-03 12:07:52 +0100

    create function js add propose

--- src/Controller/ProposeController.php
+++ src/Controller/ProposeController.php
@@ -8,17 +8,21 @@ class ProposeController extends AbstractController
 {
     public function addPropose(): string
     {
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            var_dump($_POST);
             $proposes = array_map('trim', $_POST['propose']);
 
             // TODO validations (length, format...)
 
             $proposeManager = new ProposeManager();
             foreach ($proposes as $content) {
-                $proposeManager->insertPropose($content, $_GET['id']);
+                $proposeManager->insertPropose($content, $_POST['activityId']);
             }
+
             header('Location: /activity/show?id=' . $_GET['id']);
         }
-        return $this->twig->render('Activity/addPropose.html.twig');
+        $activityId = $_GET['id'];
+        return $this->twig->render('Activity/addPropose.html.twig', ['activity_id' => $activityId]);
     }
 }
