bcedfb68f0befdf676516571b8fc40c9e906fb5a
anthony laurent <anthony061214@gmail.com>
2021-11-08 09:23:01 +0100

    create activity page

--- src/Model/ActivityManager.php
+++ src/Model/ActivityManager.php
@@ -9,10 +9,11 @@ class ActivityManager extends AbstractManager
     public function insert(array $activities): int
     {
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . "
-        (title, description, created_at) 
-         VALUES (:title, :description, now())");
+        (title, description, created_at, user_id) 
+         VALUES (:title, :description, now(), :user_id)");
         $statement->bindValue(':title', $activities['title'], \PDO::PARAM_STR);
         $statement->bindValue(':description', $activities['description'], \PDO::PARAM_STR);
+        $statement->bindValue(':user_id', $activities['user_id'], \PDO::PARAM_INT);
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
     }
