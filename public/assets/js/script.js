4a318ca6459ab4a607ad0e781a71cb1541c75ea6
Karine <k.challier@yahoo.fr>
2021-11-09 20:53:44 +0100

    add limitation of propositions

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -11,4 +11,9 @@ function addInput()
     input.setAttribute("class", 'form-control border-0 border-bottom mb-2');
     paragraph.appendChild(input);
     container.appendChild(paragraph);
+    let totalPropose = id + 1;
+    if (totalPropose >= 6) {
+        const addPropose = document.getElementById('add-propose');
+        addPropose.innerHTML = "Vous ne pouvez plus ajouter d'option";
+    } 
 }
