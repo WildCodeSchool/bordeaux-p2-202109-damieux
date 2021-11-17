6331f62fae142b9a285cf824826c157f8da8f967
anthony laurent <anthony061214@gmail.com>
2021-11-17 11:38:49 +0100

    modif responsive

--- src/routes.php
+++ src/routes.php
@@ -7,15 +7,12 @@ return [
     //'items/show'              => ['ItemController', 'show', ['id']],
     //'items/add'               => ['ItemController', 'add',],
     //'items/delete'            => ['ItemController', 'delete',],
-    ''                          => ['RegisterController', 'connect'],
+    ''                           => ['RegisterController', 'connect'],
     'activite/ajout-activite'    => ['ActivityController', 'add',],
-    'activite/ajout-comment'    => ['ActivityController', 'addCommentByActivity', ['id']],
+    'activite/ajout-comment'     => ['ActivityController', 'addCommentByActivity', ['id']],
     'activite/ajout-proposition' => ['ProposeController', 'addPropose',],
     'activite/afficher'          => ['ActivityController', 'show', ['id'],],
-    'activite/supprimer'         => ['ActivityController', 'deleteActivity', ['id'],],
-
-    'activite/modif-active'     => ['ActivityController', 'updateActivityByIsActive', ['id']],
-
+    'activite/modif-active'      => ['ActivityController', 'updateActivityByIsActive', ['id']],
     'activite/tout-afficher'     => ['ActivityController', 'showAll', ['id'],],
     'user/inscription'           => ['RegisterController', 'add',],
     'deconnecter'                => ['RegisterController', 'logout'],
